28823785d6e74781d222ed02b3fe059389efe6c7
백세영 <qortp0@naver.com>
2023-11-03 14:13:36 +0900

    최초 project commit

---
+++ src/main/java/com/example/backofficeproject/BackofficeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.backofficeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeProjectApplication.class, args);
+    }
+
+}
